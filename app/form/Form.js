8d6013a95c058089d3d40752f326c603c9aba862
Alain D'EURVEILHER <alain.deurveilher@gmail.com>
2023-06-09 23:24:16 +0200

    Update Form.js

--- app/form/Form.js
+++ app/form/Form.js
@@ -3,6 +3,8 @@
 export default function Form() {
   return (
     <form>
+    <label htmlFor="data">Data</label>
+      <input type="text" name="data" id="data" />
       <button type="submit">
         Submit
       </button>
